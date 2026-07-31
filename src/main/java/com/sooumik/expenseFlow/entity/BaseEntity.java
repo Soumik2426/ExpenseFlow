617140336771df9cb09adba874597e38ef1a5d51
Soumik2426 <soumikmaity2426@gmail.com>
2026-07-31 21:27:59 +0530

    Changed the type of createdAt and updatedAt so that it can store timezone info as well

--- src/main/java/com/sooumik/expenseFlow/entity/BaseEntity.java
+++ src/main/java/com/sooumik/expenseFlow/entity/BaseEntity.java
@@ -15,6 +15,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -31,9 +32,9 @@ public abstract class BaseEntity {
 
     @CreatedDate
     @Column(nullable = false, updatable = false)
-    private LocalDateTime createdAt;
+    private Instant createdAt;
 
     @LastModifiedDate
     @Column(nullable = false)
-    private LocalDateTime updatedAt;
+    private Instant updatedAt;
 }
