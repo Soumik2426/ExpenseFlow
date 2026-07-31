4b9b7a8e4110962428e5cb508de3458f6544b100
Soumik2426 <soumikmaity2426@gmail.com>
2026-07-31 20:00:46 +0530

    Initial Project Setup

---
+++ src/main/java/com/sooumik/expenseFlow/ExpenseFlowApplication.java
@@ -0,0 +1,11 @@
+package com.sooumik.expenseFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseFlowApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseFlowApplication.class, args);
+	}
+}
