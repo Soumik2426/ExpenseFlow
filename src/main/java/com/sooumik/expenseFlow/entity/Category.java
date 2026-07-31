8e48dc87ff7dcc5b3fb747c341270bc2cf03336f
Soumik2426 <soumikmaity2426@gmail.com>
2026-07-31 21:36:26 +0530

    Implement user and category entities

---
+++ src/main/java/com/sooumik/expenseFlow/entity/Category.java
@@ -0,0 +1,37 @@
+package com.sooumik.expenseFlow.entity;
+
+import com.soumik.expenseflow.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category extends BaseEntity {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_category_id")
+    private Category parentCategory;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(length = 255)
+    private String icon;
+}
--- src/main/java/com/sooumik/expenseFlow/entity/User.java
+++ src/main/java/com/sooumik/expenseFlow/entity/User.java
@@ -1,4 +1,25 @@
 package com.sooumik.expenseFlow.entity;
 
-public class User {
+import com.soumik.expenseflow.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+    @Column(nullable = false, unique = true, length = 150)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Column(nullable = false, length = 100)
+    private String name;
 }
