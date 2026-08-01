dc491f16fa5af0ac79dfd2d424d44522e93e4611
Soumik2426 <soumikmaity2426@gmail.com>
2026-08-01 16:38:46 +0530

    Setup the template for response and error

--- src/main/java/com/sooumik/expenseFlow/advice/ApiError.java
+++ src/main/java/com/sooumik/expenseFlow/advice/ApiError.java
@@ -1,4 +1,24 @@
 package com.sooumik.expenseFlow.advice;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ApiError {
+
+    private HttpStatus status;
+
+    private String message;
+
+    private List<String> subErrors;
 }
--- src/main/java/com/sooumik/expenseFlow/advice/ApiResponse.java
+++ src/main/java/com/sooumik/expenseFlow/advice/ApiResponse.java
@@ -1,4 +1,41 @@
 package com.sooumik.expenseFlow.advice;
 
-public class ApiResponse {
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ApiResponse<T> {
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
+
+    private T data;
+    private String message;
+    private ApiError error;
+
+    public ApiResponse() {
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public ApiResponse(T data) {
+        this();
+        this.data = data;
+    }
+
+    public ApiResponse(T data, String message) {
+        this();
+        this.data = data;
+        this.message = message;
+    }
+
+    public ApiResponse(ApiError error) {
+        this();
+        this.error = error;
+    }
 }
