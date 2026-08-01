925762c7319e6362ff22634b7e5c3cfd1973143f
Soumik2426 <soumikmaity2426@gmail.com>
2026-08-01 13:20:53 +0530

    Added a file which will prevent hardcoding of API endpoints in controller layer

---
+++ src/main/java/com/sooumik/expenseFlow/common/constants/ApiConstants.java
@@ -0,0 +1,10 @@
+package com.sooumik.expenseFlow.common.constants;
+
+public class ApiConstants {
+    private ApiConstants() {}
+
+    public static final String API_BASE_PATH = "/api/v1";
+    public static final String EXPENSES = "/expenses";
+    public static final String CATEGORIES = "/categories";
+    public static final String BUDGETS = "/budgets";
+}
