62f7d8a2fd7fc785ce5e700a87c38421376d0529
Soumik2426 <soumikmaity2426@gmail.com>
2026-08-01 13:18:29 +0530

    Implemented standardize API responses

---
+++ src/main/java/com/sooumik/expenseFlow/common/constants/ApiMessages.java
@@ -0,0 +1,10 @@
+package com.sooumik.expenseFlow.common.constants;
+
+public class ApiMessages {
+    private ApiMessages() {
+    }
+
+    public static final String EXPENSE_CREATED = "Expense created successfully.";
+    public static final String EXPENSE_UPDATED = "Expense updated successfully.";
+    public static final String EXPENSE_DELETED = "Expense deleted successfully.";
+}
---
+++ src/main/java/com/sooumik/expenseFlow/controller/ExpenseController.java
@@ -0,0 +1,7 @@
+package com.sooumik.expenseFlow.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExpenseController {
+}
